2f90d20c50fb378985d78ac7ccf99eec46f0b66f
sgabre <godefroy.sieffert>
2025-03-29 14:07:25 +0100

    Update Unit Testing

---
+++ UnitTesting/ShellSuite/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /Users/sgabre/git/TestHarness/UnitTesting/ShellSuite
+# Build directory: /Users/sgabre/git/TestHarness/UnitTesting/ShellSuite
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Shell_ParseCommand_Suite "ShellParseCommandSuiteApp")
+set_tests_properties(Shell_ParseCommand_Suite PROPERTIES  _BACKTRACE_TRIPLES "/Users/sgabre/git/TestHarness/UnitTesting/ShellSuite/CMakeLists.txt;26;add_test;/Users/sgabre/git/TestHarness/UnitTesting/ShellSuite/CMakeLists.txt;0;")
+add_test(Shell_Gets_Suite "ShellGetsSuiteApp")
+set_tests_properties(Shell_Gets_Suite PROPERTIES  _BACKTRACE_TRIPLES "/Users/sgabre/git/TestHarness/UnitTesting/ShellSuite/CMakeLists.txt;30;add_test;/Users/sgabre/git/TestHarness/UnitTesting/ShellSuite/CMakeLists.txt;0;")
+add_test(Shell_SearchCommand_Suite "ShellSearchCommandSuiteApp")
+set_tests_properties(Shell_SearchCommand_Suite PROPERTIES  _BACKTRACE_TRIPLES "/Users/sgabre/git/TestHarness/UnitTesting/ShellSuite/CMakeLists.txt;34;add_test;/Users/sgabre/git/TestHarness/UnitTesting/ShellSuite/CMakeLists.txt;0;")
